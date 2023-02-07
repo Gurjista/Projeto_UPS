1370cbecedfe7b1251699547560b636a08280f0e
João Prado <59054486+WinterDP@users.noreply.github.com>
2023-02-07 03:25:55 -0300

    add scriptable object

---
+++ UPS/Assets/Scripts/Scriptable Objects/BuildingAtributtes.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BuildingAtributtes", menuName = "UPS/BuildingAtributtes", order = 0)]
+public class BuildingAtributtes : ScriptableObject {
+    
+    public new string name;
+    
+    public string Block;
+    public List<string> nicknames;
+
+}
