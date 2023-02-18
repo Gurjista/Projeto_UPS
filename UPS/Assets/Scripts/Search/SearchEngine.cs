5d474da56907d6b27416be260129236f0e1163e7
Rodrigo Silva <rodrigowstudy@gmail.com>
2023-02-18 18:10:54 -0300

    [ADD] Sistema de busca inicial

---
+++ UPS/Assets/Scripts/Search/SearchEngine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEditor.UI;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace Search
+{
+    //Script para controlar a search engine, sera utilizado um Singleton pro enquanto. 
+    public class SearchEngine : MonoBehaviour
+    {
+        #region Fields and Properties
+        
+        #endregion
+        
+        
+        private void Awake()
+        {
+            var allBuildings = FindObjectsOfType<Construction>();
+            
+        }
+
+
+        #region Callbacks
+        // public void OnTypedCharacter(string value)
+        // {
+        //     
+        // }
+        #endregion
+        
+    }
+}
