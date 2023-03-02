2f46b313eaf863dac1c5c47a75c9411cb6855497
João Prado <59054486+WinterDP@users.noreply.github.com>
2023-03-01 21:26:58 -0300

    bugfix

---
+++ UPS/Assets/Scripts/Buildings/MaintenanceRoom.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaintenanceRoom : Room
+{
+    //[Header("informações aula")]
+
+    public override BuildType BuildType => BuildType.Sala_de_Manutencao;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
