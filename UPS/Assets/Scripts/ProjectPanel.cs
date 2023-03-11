26c768f6bde3bc7de8674c6be66ced58b087d857
Gustavo Rocha <gustavo.prietorocha1904@gmail.com>
2023-03-11 18:22:41 -0300

    ProjectRoom script 2.0
    Finalizado script que localiza sala de projeto a partir de ProjectPanel

--- UPS/Assets/Scripts/ProjectPanel.cs
+++ UPS/Assets/Scripts/ProjectPanel.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System.Linq;
 
 public class ProjectPanel : MonoBehaviour
 {
     [SerializeField] private string _projectRoom;
-    [SerializeField] private GameObject _mapButton;
+    [SerializeField] private Button _mapButton;
     // Start is called before the first frame update
     void Start()
     {
-        var project = FindObjectOfType<ProjectRoom>();
-        var aux = project.First(x => x.name == _projectRoom);
-        aux.GetComponent<Button>().onClick.AddListener(project.HighlightConstruction);
+        var project = FindObjectsOfType<ProjectRoom>();
+        var aux = project.First(x => x.Name == _projectRoom);
+        _mapButton.onClick.AddListener(aux.HighlightConstruction);
+        Debug.Log("Nome aux: " + aux.name);
     }
 
    
