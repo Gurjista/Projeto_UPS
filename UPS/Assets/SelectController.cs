89988f3a588ccde47b2a9089a13b5b0b982e2d37
João Prado <59054486+WinterDP@users.noreply.github.com>
2023-03-23 15:51:01 -0300

    build 23_03

--- UPS/Assets/SelectController.cs
+++ UPS/Assets/SelectController.cs
@@ -12,7 +12,7 @@ public class SelectController : MonoBehaviour
     public Action deselectCallback = null;
     public int maxDist;
     public Button x;
-    public Button copy_email;
+    //public Button copy_email;
 
     private GraphicRaycaster GR;
     private EventSystem ES;
@@ -22,7 +22,7 @@ public class SelectController : MonoBehaviour
     void Start()
     {
         x.gameObject.SetActive(false);
-        copy_email.gameObject.SetActive(false);
+        //copy_email.gameObject.SetActive(false);
         GR = GameObject.Find("MainCanvas").GetComponent<GraphicRaycaster>();
         ES = GameObject.Find("EventSystem").GetComponent<EventSystem>();
         //cameraT = Camera.main.transform;
